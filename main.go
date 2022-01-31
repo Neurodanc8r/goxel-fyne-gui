e28cca18e502f3368730429a0749b742dc5aa752
Evgeniy <evgeniy.korzyuk@gmail.com>
2022-01-31 18:42:40 +1000

    resize window

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
 )
 
 func main() {
-	a := app.New()                 // Инициализация приложения
-	w := a.NewWindow("Hello Fyne") // Создание окна
+	a := app.New()                   // Инициализация приложения
+	w := a.NewWindow("Hello Fyne")   // Создание окна
+	w.Resize(fyne.NewSize(400, 400)) // Задаем размер окна
 
 	w.SetContent(widget.NewLabel("Hello World!")) // Наполняем контентом
 	w.ShowAndRun()                                // Запуск окна
